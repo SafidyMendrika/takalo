b8adfb61c369486faac31be06183f705fdb5c5c6
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-02-08 01:25:28 +0300

    accueil dynamisé

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,6 +7,9 @@ class Home extends Session_Secure {
     public function __construct()
     {
         parent::__construct();
+        $this->load->model("DAO_model");
+        $this->load->model("PDO_Connector");
+        $this->load->model("Object_model");
     }
      public function index()
     {
@@ -15,10 +18,28 @@ class Home extends Session_Secure {
 
     public function adminHome(){
         $this->checkSession("admin",base_url("login/adminLoginPage"));
-        $this->load->view("admin");
+
+        $sess = $this->session->admin;
+        $data["admin_name"] = $sess["password"];
+
+        $objectModel = new Object_model();
+
+        $data["objects"] = $objectModel->getAllWithDetals();
+
+        $this->load->view("homeadmin",$data);
     }
     public function clientHome(){
         $this->checkSession("user",base_url("login/"));
-    $this->load->view("accueil");
+
+        $sess = $this->session->user;
+        $data["user_name"] = $sess["password"];
+        $data["user_id"] = $sess["id"];
+
+        $objectModel = new Object_model();
+
+        $data["objects"] = $objectModel->getAllWithDetals();
+
+
+        $this->load->view("accueil",$data);
 }
 }
