a17e9a7c875fa8c8049a0e1326cb2c99568096d5
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-02-07 15:21:45 +0300

    update base

--- application/script/base.sql
+++ application/script/base.sql
@@ -1,25 +1,57 @@
-create database takalo;
+    create database takalo;
 
-use takalo;
+    use takalo;
 
-CREATE TABLE user (
-    id int primary key auto_increment,
-    email varchar(255),
-    password varchar(255),
-    isAdmin boolean
-);
+    drop table user;
+    drop table photos;
+    drop table categorie;
+    drop table objet;
+    drop table details_objet;
+    drop table proposition;
 
-CREATE TABLE categorie (
-    id int primary key auto_increment,
-    nom varchar(255)
-);
+    CREATE TABLE user (
+        id int primary key auto_increment,
+        email varchar(255) not null,
+        password varchar(255) not null,
+        isAdmin boolean
+    );
 
-CREATE TABLE objet (
-    id int primary key auto_increment,
-    idUser int,
-    idCategorie int,
-    nom varchar(255),
-    prix float,
-    foreign key(idUser) references user(id),
-    foreign key(idCategorie) references categorie(id)
-);
+
+    CREATE TABLE photos(
+        id int primary key auto_increment,
+        idObjet int not null,
+        link varchar(255),
+        foreign key(idObjet) references objet(id)
+    );
+
+    CREATE TABLE categorie (
+        id int primary key auto_increment,
+        nom varchar(255) not null
+    );
+
+    CREATE TABLE objet (
+        id int primary key auto_increment,
+        idUser int not null,
+        nom varchar(255),
+        prix float,
+        foreign key(idUser) references user(id),
+    );
+
+    CREATE TABLE details_objet VALUES (
+        id int primary key ,
+        idObjet int not null,
+        idCategorie int default 6,
+        foreign key (idObjet) references objet(id),
+        foreign key (idCategorie) references categorie(id)
+    );
+
+    CREATE TABLE proposition(
+        id int primary key auto_increment,
+        idUser int not null,
+        idObjetUser int not null,
+        idObjet int not null,
+        status varchar(255),
+        foreign key (idUser) references user(id),
+        foreign key (idObjetUser) references user(id), 
+        foreign key (idObjet) references objet(id)
+    );
