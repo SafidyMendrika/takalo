344431b531778bf320a3c6446c86cccdf4bd0a82
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-02-07 13:50:26 +0300

    fonctions login

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,6 +1,7 @@
 <?php
 $this->load->model("DAO_model");
 $this->load->model("PDO_Connector");
+$this->load->model("Login_model");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,6 +25,13 @@ $connection = $connector->connect();
 $allPopo = DAO_model::selectAll($connection,"popo"," name='popo' ");
 
 var_dump($allPopo);
+
+$log = new Login_model();
+$adm = $log->logAsAdmin("men@gmail.com","azerty");
+
+var_dump($adm);
 ?>
+
+    <a href="<?php echo base_url("login") ?>"> log</a>
 </body>
 </html>
